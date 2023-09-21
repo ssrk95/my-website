d07362c3d6029339afedbce0feb6257d4d9ee823
Sriram G <gsriramsgkrishnan@gmail.com>
2023-09-21 22:54:27 +0530

    skills refinement - minor UI refinement

--- src/components/Details.js
+++ src/components/Details.js
@@ -11,13 +11,14 @@ function Details({section}){
                         <li>CSS</li>
                         <li>HTML5</li>
                         <li>HTML Canvas</li>
-                        <li>Web Audio API</li>
+                        <li>Web API</li>
                         <li>Immutable Js</li>
                         <li>Agile Development Process</li>
                         <li>Atlassian Jira</li>
                         <li>Gitlab</li>
                         <li>Github</li>
                         <li>Visual Studio Code</li>
+                        <li>Jenkins (CI/CD)</li>
                     </ol>
 
     return(
