2c9503aae62ebf99cecb727d5621101c189ad2d5
Sriram G <gsriramsgkrishnan@gmail.com>
2024-03-25 21:05:01 +0530

    Self intro minor refinement

--- src/components/SelfIntro.js
+++ src/components/SelfIntro.js
@@ -3,7 +3,7 @@ function SelfIntro(){
     return(
         <div className="selfIntro">
             <p>
-                I'm a passionate Front-end developer with 4 years 6 months of experience spanning deep React JS programming, Client-side development & Data analysis.
+                I'm a passionate Frontend developer with 4 years 8 months of experience spanning deep React JS programming, Client-side development & Data analysis.
             </p>
             <p>
                 Building fascinating Front-ends and ardent problem solving make my day.
